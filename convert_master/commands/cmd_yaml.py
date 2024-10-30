37811669e9e94e44e44ee8020d2462153765cf68
Nikita Yakovlev <nikita.yakovlev@spl.games>
2024-10-30 22:25:46 +0300

    transferring yaml saving to a module with the command

--- convert_master/commands/cmd_yaml.py
+++ convert_master/commands/cmd_yaml.py
@@ -37,11 +37,17 @@ def get_output_path(opts):
     return yaml_path
 
 
-def run_convert(opts):
+def run_convert(opts) -> None:
     json_file = get_json_path(opts)
     yaml_file = get_output_path(opts)
     if not validate_files(json_file, yaml_file):
         return
 
     convert = Converting(input_file=json_file, output_file=yaml_file)
-    return convert.convert_json_to_yaml()
+    yaml_string = convert.convert_json_to_yaml()
+
+    with open(yaml_file, "w", encoding='utf-8') as file:
+        file.write(yaml_string)
+    logger.info(f"The JSON from {json_file} was converted to YAML and saved in {yaml_file}")
+
+
--- convert_master/converting.py
+++ convert_master/converting.py
@@ -1,7 +1,6 @@
 import base64
 import json
 import logging
-import os
 
 import yaml
 
@@ -14,14 +13,10 @@ class Converting:
         self.input_file = input_file
         self.output_file = output_file
 
-    def convert_json_to_yaml(self) -> None:
+    def convert_json_to_yaml(self) -> str:
         with open(self.input_file, "r", encoding='utf-8') as file:
             json_dict = json.load(file)
-        yaml_string = yaml.dump(json_dict, sort_keys=False)
-
-        with open(self.output_file, "w", encoding='utf-8') as file:
-            file.write(yaml_string)
-        logger.info(f"The JSON from {self.input_file} was converted to YAML and saved in {self.output_file}")
+        return yaml.dump(json_dict, sort_keys=False)
 
     def convert_image_to_base64(self) -> str:
         with open(self.input_file, "rb") as image:
